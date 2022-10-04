b620f12b68ffdb86e59b494a1e5520312772fcfb
RiTZ <RiTZ@DESKTOP-IS8E4DN>
2022-10-04 11:39:05 +0530

    first commit

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,26 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Test {
 
 	public static void main(String[] args) 
 	{
-		System.setProperty("webdriver.chrome.driver", "");
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://opensource-demo.orangehrmlive.com/");
-		
-		By username = By.name("txtUsername");
-		WebElement searchbox=driver.findElement(username);
-		searchbox.sendKeys("Admin");
-		
-		
-		By password=By.name("txtPassword");
-		WebElement passwordbox=driver.findElement(password);
-		passwordbox.sendKeys("admin123");
-		
-		driver.close();
-		
 
+	
+		{
+			String hungry="yes";
+			if(hungry == "yes")
+			{
+				System.out.println("eat something");
+				
+			}
+			else
+			{
+				System.out.println("do work");
+			}
+		
+		}
 	}
-
 }
+	
+
+
